a68b2635b798b61e9fb987ad1b92b5589c5b3bad
Walter Kuppens <reshurum@gmail.com>
2016-05-21 22:10:49 -0700

    Added proper argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate byteorder;
+extern crate getopts;
 extern crate rand;
 extern crate sdl2;
 extern crate time;
@@ -7,6 +8,8 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+
+use getopts::Options;
 
 mod cpu;
 mod graphics;
@@ -17,31 +20,45 @@ mod sound;
 mod vm;
 
 fn main() {
-    // TODO: Use an actual argument parser when adding options in the future.
-    if let Some(rom_file_name) = env::args().nth(1) {
-        let rom = read_bin(rom_file_name);
+    let args: Vec<String> = env::args().collect();
 
-        let mut vm = vm::VirtualMachine::new(rom);
-        vm.run();
-    } else {
-        print_usage();
-        std::process::exit(1);
+    // Initialize the argument parser and parse them.
+    let mut opts = Options::new();
+    opts.optflag("v", "version", "print version information");
+    opts.optflag("h", "help", "Print this message");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            println!("{}\n", f.to_string());
+            print_usage(opts);
+            std::process::exit(1);
+        },
+    };
+
+    // Argument flags.
+    if matches.opt_present("v") {
+        print_version();
+        return;
     }
-}
+    if matches.opt_present("h") {
+        print_usage(opts);
+        return;
+    }
 
-/// Prints the application name alongside the cargo version.
-fn print_version() {
-    println!("notch {}", env!("CARGO_PKG_VERSION"));
-}
+    // Assume the first free argument is the rom filename.
+    let rom_file_name = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(opts);
+        std::process::exit(1);
+    };
 
-/// Prints usage information.
-fn print_usage() {
-    println!("Notch is a CHIP-8 virtual machine written in Rust.");
-    println!("");
-    println!("Usage: notch [ROM]");
-    println!("");
-    println!("To contribute or report bugs, please see:");
-    println!("<https://github.com/Reshurum/notch>");
+    let rom = read_bin(&rom_file_name);
+    println!("Loading rom: {}", rom_file_name);
+
+    // Initialize the virtual machine and boot the rom.
+    let mut vm = vm::VirtualMachine::new(rom);
+    vm.run();
 }
 
 /// Reads a file into a vector of unsigned bytes.
@@ -61,4 +78,18 @@ fn read_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
     };
 
     buffer
+}
+
+/// Prints the application name alongside the cargo version.
+fn print_version() {
+    println!("notch {}", env!("CARGO_PKG_VERSION"));
+}
+
+/// Prints usage information.
+fn print_usage(opts: Options) {
+    println!("Notch is a CHIP-8 virtual machine written in Rust.");
+    println!("");
+    println!("{}", opts.usage("Usage: notch [OPTIONS] ROM"));
+    println!("To contribute or report bugs, please see:");
+    println!("<https://github.com/Reshurum/notch>");
 }
